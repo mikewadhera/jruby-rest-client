089da8b0afc67e0f513a8de2a7c03222e96722dd
Mike Wadhera <mikewadhera@casper.local>
2009-05-10 22:15:48 -0700

    Referencing java.lang.System directlt instead of using include_class

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 if defined? JRUBY_VERSION
-  include_class "java.lang.System"
-  System.setProperty "org.apache.commons.logging.Log",
-                     "org.apache.commons.logging.impl.NoOpLog"  
+  # Disable Apache Commons logging
+  java.lang.System.setProperty "org.apache.commons.logging.Log",
+                               "org.apache.commons.logging.impl.NoOpLog"  
   require "jruby_restclient"
 end
